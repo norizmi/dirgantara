e2e15fa0e3949bf81ee8f7c99ec75dc7a1752e4b
Naufal <naufalnaufal841@gmail.com>
2024-06-25 14:39:31 +0700

    update-drawer

--- src/components/Fragments/Header.jsx
+++ src/components/Fragments/Header.jsx
@@ -1,9 +1,9 @@
-import logoDigantara from '../assets/images/logo-dirgantara.png';
-import { useState, useEffect, useRef } from 'react';
+import logoDigantara from "../assets/images/logo-dirgantara.png";
+import { useState, useEffect, useRef } from "react";
+import menu from "../assets/icon/menu.svg";
 
 function Header() {
   const [isFixed, setIsFixed] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const menuRef = useRef(null);
 
   useEffect(() => {
@@ -16,58 +16,60 @@ function Header() {
       }
     };
 
-    const handleClickOutside = (event) => {
-      if (menuRef.current && !menuRef.current.contains(event.target)) {
-        setIsMenuOpen(false);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    document.addEventListener('mousedown', handleClickOutside);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
-      document.removeEventListener('mousedown', handleClickOutside);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
-    <header className='transition-all duration-300 shadow-lg'>
-      <div className={`navbar top-0 left-0 right-0 bg-[#C7C7C7] z-10 ${
-        isFixed ? 'fixed shadow-md' : 'sticky'
-      }`}>
-        <div className='container mx-auto flex justify-around items-center'>
+    <header className="transition-all duration-300 shadow-lg">
+      <div
+        className={`navbar top-0 left-0 right-0 bg-[#C7C7C7] z-10 ${
+          isFixed ? "fixed shadow-md" : "sticky"
+        }`}
+      >
+        <div className="container mx-auto flex justify-around items-center">
           <div className="flex-1">
-            <img src={logoDigantara} alt="logo-dirgantara"/>
+            <img src={logoDigantara} alt="logo-dirgantara" />
           </div>
           <div className="flex-none relative" ref={menuRef}>
-            <button className="btn btn-ghost btn-info" onClick={toggleMenu}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block w-5 h-5 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
-            </button>
-            {isMenuOpen && (
-              <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-20">
-                <a href="#about" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Role in Game</a>
-                <a href="#services" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">How to Play</a>
-                <a href="#contact" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Dirgantara Products</a>
-                <a href="#contact" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Contact</a>
+            <div className="drawer drawer-end">
+              <input
+                id="my-drawer-4"
+                type="checkbox"
+                className="drawer-toggle"
+              />
+              <div className="drawer-content">
+                <label htmlFor="my-drawer-4" className="drawer-button ">
+                  <img src={menu} alt="menu" className="w-7 mr-1" />
+                </label>
               </div>
-            )}
+              <div className="drawer-side">
+                <label
+                  htmlFor="my-drawer-4"
+                  aria-label="close sidebar"
+                  className="drawer-overlay"
+                ></label>
+                <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                  <div className="flex flex-col items-center mt-10">
+                    <li>
+                      <a>Role in Game</a>
+                    </li>
+                    <li>
+                      <a>How to Play</a>
+                    </li>
+                    <li>
+                      <a>Dirgantara Products</a>
+                    </li>
+                    <li>
+                      <a>Contact</a>
+                    </li>
+                  </div>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
